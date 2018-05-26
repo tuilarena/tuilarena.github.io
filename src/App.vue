5748c47577ee3930a622cf2790f05ab55cc11de0
Børge Warvik <bowarv@gmail.com>
2018-05-26 16:59:10 +0200

    Basic Vue project installed

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <SiteHeader></SiteHeader>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import SiteHeader from '@/components/SiteHeader'
+export default {
+  components: {
+    SiteHeader
+  },
+  name: 'App'
+};
+</script>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,17 @@
+<template>
+	<header class="header">
+	    <div class="container-fluid">
+	        <div class="row align-items-center">
+	            <div class="col-8 p-3 pl-4">
+	                <img src="../assets/tuil-logo.svg" style="width: 40px;">
+	            </div>
+	            <div class="col-4 text-right text-white pr-4">
+	                <span class="border-transparent">Bli arenaløfter!</span>
+	                <a href="https://butikk.tuil.no/categories/arena-ii" target="_blank" class="text-white border border-white rounded p-2 ml-3">Privatperson</a>
+	                <a href="#" class="text-white border border-white rounded p-2 ml-3">Bedrift</a>
+	            </div>
+	        </div>
+	    </div>
+	</header>
+</template>
+<style lang="scss" scoped></style>
