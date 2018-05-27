73f1b3821256914b91923966778885278ccb12d3
Børge Warvik <bowarv@gmail.com>
2018-05-27 13:19:29 +0200

    Working on mobile menu

---
+++ src/components/OverlayMenu.vue
@@ -0,0 +1,112 @@
+<template>
+<nav>
+	<div class="container-fluid">
+		<div class="row mt-4">
+			<div class="col text-right pr-4">
+				<i class="fas fa-times" @click="$emit('close')"></i>
+			</div>
+		</div>
+
+		<div class="row mb-2">
+			<div class="col text-center pr-4 pl-4">
+				<img src="../assets/tuil-logo.svg" style="width: 5rem;" class="mb-3">
+				<h4>Støtt TUIL og bli en arenaløfter!</h4>
+				<p>
+					Vi er glade for å kunne tilby en markedsarena der alle våre medspillere kan bidra til utviklingen i TUIL og Tromsdalen. Vi serverer gode opplevelser og inspirerer til økt lønnsomhet for våre medspillere.
+				</p>
+			</div>
+		</div>
+
+		<div class="row mb-5">
+			<div class="col-12 text-center pr-4 pl-4 support-as-text">
+				<p>
+					Støtt TUIL som
+				</p>
+			</div>
+			<div class="col pl-4">
+				<a href="#" class="btn btn-primary btn-block">Privatperson</a>
+			</div>
+			<div class="col pr-4">
+				<a href="#" class="btn btn-primary btn-block">Bedrift</a>
+			</div>
+		</div>
+
+		<div class="row mb-5">
+			<div class="col pr-4 pl-4">
+				<ul>
+					<li><a href="#" class="text-white">Se hvem som allerede har kjøpt</a></li>
+					<li><a href="#" class="text-white">TUIL takker disse bedriftene</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row mb-5">
+			<div class="col">
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+				<p>hei</p>
+			</div>
+		</div>
+	</div>
+</nav>
+</template>
+<script>
+export default {
+	methods: {
+		close: () => {
+			console.log(this)
+		}
+	}
+};
+</script>
+<style lang="scss" scoped>
+$blue: rgb(35,46,86);
+$yellow: rgb(255,166,17);
+$white_bg: #f7f7f7;
+
+$font-family: 'Roboto', sans-serif;
+
+nav {
+	position: fixed;
+	overflow: auto;
+	background-color: #222a46;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	z-index: 1000;
+	color: #fff;
+
+	.support-as-text {
+		font-weight: 100;
+		text-transform: uppercase;
+		font-size: .8rem;
+	}
+
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+
+		li {
+			margin: 0;
+		}
+	}
+}
+
+</style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,26 +1,81 @@
 <template>
 	<header class="header">
+		<OverlayMenu v-if="showOverlayMenu" @close="showOverlayMenuj = false"></OverlayMenu>
 	    <div class="container-fluid">
 	        <div class="row align-items-center">
-	            <div class="col-8 p-3 pl-4">
+	            <div class="col p-3 pl-4">
 	                <img src="../assets/tuil-logo.svg" style="width: 40px;">
 	            </div>
-	            <div class="col-4 text-right text-white pr-4">
-	                <span class="border-transparent">Bli arenaløfter!</span>
-	                <a href="https://butikk.tuil.no/categories/arena-ii" target="_blank" class="text-white border border-white rounded p-2 ml-3">Privatperson</a>
-	                <a href="#" class="text-white border border-white rounded p-2 ml-3">Bedrift</a>
+	            <div class="col text-right text-white pr-4">
+
+					<i class="fas fa-bars" @click="showOverlayMenu = !showOverlayMenu"></i>
+
+	                <div class="menu">
+	                	<span class="border-transparent">Bli arenaløfter!</span>
+	                	<a href="https://butikk.tuil.no/categories/arena-ii" target="_blank" class="text-white border border-white rounded p-2 ml-3">Privatperson</a>
+	                	<a href="#" class="text-white border border-white rounded p-2 ml-3">Bedrift</a>
+	                </div>
 	            </div>
 	        </div>
 	    </div>
 	</header>
 </template>
+<script>
+import OverlayMenu from './OverlayMenu'
+export default {
+	components: {
+		OverlayMenu
+	},
+
+	data() {
+		return {
+			showOverlayMenu: true
+		}
+	}
+};
+</script>
 <style lang="scss" scoped>
 $blue: rgb(35,46,86);
 $yellow: rgb(255,166,17);
 $white_bg: #f7f7f7;
 
 $font-family: 'Roboto', sans-serif;
-	header {
-	    background-color: #222a46;
-	}
+header {
+    background-color: #222a46;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    background-color: #222a46;
+
+    .menu {
+    	display: none;
+    }
+}
+
+@media all and (min-width:576px) {
+    
+
+}
+
+@media all and (min-width:768px) {
+    
+}
+
+@media all and (min-width:992px) {
+    .fa-bars {
+    	display: none;
+    }
+
+	.menu {
+    	display: block !important;
+    }
+}
+
+@media all and (min-width:1200px) {
+    
+}
 </style>
